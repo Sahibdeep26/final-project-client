c37c1ae273cd7811641d0d14e2e816eedf158258
Sahibdeep26 <59907830+Sahibdeep26@users.noreply.github.com>
2021-03-31 19:11:39 -0700

    update

--- js/writeDB.js
+++ js/writeDB.js
@@ -1,35 +1,7 @@
 function resetAddForm() {
   document.getElementById('addform').reset();
 }
-/*
-const addQuestion = () => {
-  let tags = document.getElementById('tags').value;
-  let name = document.getElementById(`name`).value;
-  let link = document.getElementById('url').value;
 
-  let formData = {
-    "link": link,
-    "title": name,
-    "publisher": tags
-}
-
-  fetch('https://final-project-server-3p9ru.ondigitalocean.app/bookmarks/create', {
-      method: 'post',
-      mode: 'cors',
-      cache: 'no-cache',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData)
-  }).then(response => response).then(data => {
-      console.log(data);
-  });
-
-  resetAddForm();
-}
-
-
-*/
 const addQuestion = () => {
   let tags = document.getElementById('tags').value;
   let name = document.getElementById(`name`).value;
@@ -62,6 +34,7 @@ const addQuestion = () => {
       }).then(response => response).then(data => {
         console.log(data);
       });
+      alert("Bookmark Saved!!!")
       resetAddForm()
     }else {
       console.log('Save Cancelled');
