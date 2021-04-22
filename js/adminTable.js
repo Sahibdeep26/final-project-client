1bd66aae19752462cf5259419d6cd64e03e30c0b
Sahibdeep26 <59907830+Sahibdeep26@users.noreply.github.com>
2021-04-22 14:30:34 -0700

    update

--- js/adminTable.js
+++ js/adminTable.js
@@ -8,34 +8,36 @@ function dispData() {
           for (let i = 0; i < data.length; i++) {
               console.log(data[i]);
               output.insertAdjacentHTML('beforeend', `
-              <tr>
-              <th scope="row">${data[i].uuid}</th>
-              <td>${data[i].publisher}</td>
-              <td>${data[i].title}</td>
-              <td>${data[i].link}</td>
-              <td>
-                <a href = "../editQuestion.html" class="btn border-shadow update" id="updateBtn" data-id="${data[i].uuid}" onclick='editFormLoader("${data[i].uuid}", "${data[i].publisher}", "${data[i].title}", "${data[i].link}")'>
-                  <span class="text-gradient"><i class="fas fa-pencil-alt"></i></span>
-                </a>
-                <a class="btn border-shadow delete"  id="deleteBtn" data-id="${data[i].uuid}" onclick="deleteQuestion(${data[i].uuid})">
-                  <span class="text-gradient"><i class="fas fa-trash-alt"></i></span>
-                </a>
-              </td>
-            </tr>
+              
+              <div class="btn-group dropend card-header">
+                <div class="col-sm-11 col-md-11 col-lg-11 text-truncate"><i class="far fa-bookmark"> &nbsp;</i> ${data[i].title}</div>
+               
+                <button type="button" class="btn btn-dark" id="options" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-ellipsis-v"></i></button>
+
+                <ul class="dropdown-menu">
+                  <li><button type="button" class="dropdown-item text-white" data-bs-toggle="modal" data-bs-target="#editBookmarkModal" id="editButton-${i}"><span class="text-gradient"><i class="fas fa-pencil-alt"></i></span>&nbsp Edit</button></li>                  
+                  <li><button class="dropdown-item text-white" type="button" data-id="${data[i].uuid}" onclick="deleteQuestion(${data[i].uuid})"><span class="text-gradient"><i class="fas fa-trash-alt"></i></span>&nbsp  Delete</button></li>
+                  <li><a href="${data[i].link}" class="btn dropdown-item text-white" target="_blank"  id="updateBtn" >Open page in new window</a></li>
+                </ul>
+              </div>
           `);
+              document.getElementById(`editButton-${i}`).addEventListener("click", () => {
+                editFormLoader(data[i].id, data[i].publisher, data[i].title, data[i].link);
+              });
           }
       });
+}
+
+function resetForms() {
+  document.getElementById('editForm').reset();
+  document.getElementById('addForm').reset();
 
 }
 
-const editFormLoader = (uuid, publisher,title,link) => {
-
-  console.log(uuid, publisher, title, link)
-  
-  document.getElementById('bookId').value = uuid;
-  document.getElementById('tags').value = publisher
-  document.getElementById('name').value = title
-  document.getElementById('link').value= link
+function editFormLoader (uuid, publisher, title, link) {
+  document.getElementById('bookTags').value = publisher;
+  document.getElementById(`bookName`).value = title ;
+  document.getElementById('bookURL').value = link;
 }
 
 function deleteQuestion(uuid) {
@@ -56,3 +58,56 @@ function deleteQuestion(uuid) {
   }
 }
 
+
+const addBookmark = () => {
+  let tags = document.getElementById('tags').value;
+  let name = document.getElementById(`name`).value;
+  let link = document.getElementById('url').value;
+
+  let formData = {
+    "link": link,
+    "title": name,
+    "publisher": tags
+  }
+
+  if(name==""){
+    alert("Please provide a name for the bookmark")
+    return false;
+  }else if(link==""){
+    alert("Please provide a URL");
+    return false;
+  }else{
+    if (confirm('Are you sure you want to save the bookmark?')) {
+        // Edit
+        console.log(formData);
+        fetch('https://final-project-server-3p9ru.ondigitalocean.app/api/v1/bookmarks/create', {
+        method: 'post',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      }).then(response => response).then(data => {
+        console.log(data);
+      });
+      alert("Bookmark Saved!!!")
+      resetForms()
+      location.reload();
+    }else {
+      console.log('Save Cancelled');
+    }
+  }
+} 
+
+
+
+function validURL(str) {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(str);
+}
